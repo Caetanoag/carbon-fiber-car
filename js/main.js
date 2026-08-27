bcfa8d7186b6d8cf4996b28e8491571abd4a3f8b
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-27 15:32:09 -0300

    fixed the path problems

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 //anotações
-/*hro :    
+/*hro :
 3 primeiras do carrossel começa falando só do carro
 corrossel é o resumo do conteudo no site
 */
 import { staticHtmlServer } from "/js/staticHtmlServer.js";
-const carrosselLoader = new staticHtmlServer("Carrossel", document.querySelector('#carrossel'), "./html/carrossel.html")
-const formLoader = new staticHtmlServer("Form", document.querySelector("#forms"), "./html/form.html")
-const navBarLoader = new staticHtmlServer("NavBar", document.querySelector('nav'), "./html/navbar.html")
-const footerLoader = new staticHtmlServer("Footer", document.querySelector('footer'), "./html/footer.html");
+
+const baseHtmlSourceUrl = `http://${location.host}${location.pathname}html`
+
+
+const carrosselLoader = new staticHtmlServer("Carrossel", document.querySelector('#carrossel'), `${baseHtmlSourceUrl}/carrossel.html`)
+const formLoader = new staticHtmlServer("Form", document.querySelector("#forms"), `${baseHtmlSourceUrl}/form.html`)
+const navBarLoader = new staticHtmlServer("NavBar", document.querySelector('nav'), `${baseHtmlSourceUrl}/navbar.html`)
+const footerLoader = new staticHtmlServer("Footer", document.querySelector('footer'), `${baseHtmlSourceUrl}/footer.html`);
 formLoader.loadElement();
 carrosselLoader.loadElement();
 navBarLoader.loadElement();
