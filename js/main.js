d197f884d84e97627121d41a99b95b8f0c0df060
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-27 15:38:13 -0300

    trying to fix the resolve

--- js/main.js
+++ js/main.js
@@ -3,15 +3,16 @@
 3 primeiras do carrossel começa falando só do carro
 corrossel é o resumo do conteudo no site
 */
-import { staticHtmlServer } from "/js/staticHtmlServer.js";
 
-const baseHtmlSourceUrl = `http://${location.host}${location.pathname}html`
+import { staticHtmlServer } from "./js/staticHtmlServer.js";
 
+const baseUrl = new URL("./html/", window.location.href).href;
 
-const carrosselLoader = new staticHtmlServer("Carrossel", document.querySelector('#carrossel'), `${baseHtmlSourceUrl}/carrossel.html`)
-const formLoader = new staticHtmlServer("Form", document.querySelector("#forms"), `${baseHtmlSourceUrl}/form.html`)
-const navBarLoader = new staticHtmlServer("NavBar", document.querySelector('nav'), `${baseHtmlSourceUrl}/navbar.html`)
-const footerLoader = new staticHtmlServer("Footer", document.querySelector('footer'), `${baseHtmlSourceUrl}/footer.html`);
+const carrosselLoader = new staticHtmlServer("Carrossel", document.querySelector('#carrossel'), `${baseUrl}carrossel.html`);
+const formLoader = new staticHtmlServer("Form", document.querySelector("#forms"), `${baseUrl}form.html`);
+const navBarLoader = new staticHtmlServer("NavBar", document.querySelector('nav'), `${baseUrl}navbar.html`);
+const footerLoader = new staticHtmlServer("Footer", document.querySelector('footer'), `${baseUrl}footer.html`);
+
 formLoader.loadElement();
 carrosselLoader.loadElement();
 navBarLoader.loadElement();
