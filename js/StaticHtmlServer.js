352865c3ffc05aaffaa4c801dc2b9ae37370e9e2
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-28 12:20:30 -0300

    Renamed static loader

---
+++ js/StaticHtmlServer.js
@@ -0,0 +1,17 @@
+export class StaticHtmlServer {
+    constructor(name, htmlSection, htmlSrcPath){
+        this.name = name;
+        this.owner = htmlSection;
+        this.src = htmlSrcPath;
+    }
+    async getSourceHtml(){
+        const response = await fetch(this.src);
+        const html = await response.text();
+        return html;
+    }
+    async loadElement(){
+        const html = await this.getSourceHtml();
+        this.owner.innerHTML = html;
+        console.log(`${this.name} loaded!`);
+    }
+}
