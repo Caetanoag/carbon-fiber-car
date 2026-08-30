a91a0f8e18faa2134e7b1ff435d37a44de80e269
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-30 13:12:43 -0300

    Added sections templates

--- js/main.js
+++ js/main.js
@@ -9,20 +9,43 @@ import { ThemeSwitcher } from "./ThemeSwitcher.js";
 
 const baseUrl = new URL("./html/", window.location.href).href;
 
-const carrosselLoader = new StaticHtmlServer("Carrossel", document.querySelector('#carrossel'), `${baseUrl}carrossel.html`);
-const formLoader = new StaticHtmlServer("Form", document.querySelector("#forms"), `${baseUrl}form.html`);
-const navBarLoader = new StaticHtmlServer("NavBar", document.querySelector('nav'), `${baseUrl}navbar.html`);
-const footerLoader = new StaticHtmlServer("Footer", document.querySelector('footer'), `${baseUrl}footer.html`);
+const carrosselLoader = new StaticHtmlServer(
+	"Carrossel",
+	document.querySelector("#carrossel"),
+	`${baseUrl}carrossel.html`,
+);
+const formLoader = new StaticHtmlServer(
+	"Form",
+	document.querySelector("#forms"),
+	`${baseUrl}form.html`,
+);
+const navBarLoader = new StaticHtmlServer(
+	"NavBar",
+	document.querySelector("nav"),
+	`${baseUrl}navbar.html`,
+);
+const footerLoader = new StaticHtmlServer(
+	"Footer",
+	document.querySelector("footer"),
+	`${baseUrl}footer.html`,
+);
+
+const carbonFiberSectionLoader = new StaticHtmlServer(
+	"Section - Carbon Fiber",
+	document.querySelector("#fibra-de-carbono"),
+	`${baseUrl}sections/section.carbon.html`,
+);
 
 async function init() {
-  await formLoader.loadElement();
-  await carrosselLoader.loadElement();
-  await navBarLoader.loadElement();
-  await footerLoader.loadElement();
-  const themeSwitcher = new ThemeSwitcher(
-    document.documentElement,
-    document.querySelector("#theme-toggle")
-  );
-  themeSwitcher.loadFromLocalStorage();
+	await formLoader.loadElement();
+	await carrosselLoader.loadElement();
+	await navBarLoader.loadElement();
+	await footerLoader.loadElement();
+	await carbonFiberSectionLoader.loadElement();
+	const themeSwitcher = new ThemeSwitcher(
+		document.documentElement,
+		document.querySelector("#theme-toggle"),
+	);
+	themeSwitcher.loadFromLocalStorage();
 }
 init();
